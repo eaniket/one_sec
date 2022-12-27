2c6cdb0b0ae0377fcfb6bd310db6e7da307218cb
aniket_406 <singhaniket406@gmail.com>
2022-12-27 15:23:37 +0530

    added share, navbar and other functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,15 +6,21 @@ const display = document.getElementsByClassName('display')[0];
 const fill = document.getElementsByClassName('fill')[0];
 const yourScore = document.getElementById('your-score-val');
 const highestScoreVal = document.getElementById('highest-score-val');
+const shareTextBox = document.getElementById('share-modal-text');
 
 const endGameModal = document.getElementById('end-game-modal');
 const instructionModal = document.getElementById('instruction-modal');
+const shareModal = document.getElementById('share-modal');
 
 let done = false;
 let score = 0;
+let firstMove = true;
+
+const hostedUrl = 'https://www.google.com';
+let sharedText;
 
 function updateDisplay() {
-	if (done) {
+	if (done && !firstMove) {
 		done = false;
 		fill.classList.remove('fill');
 		setTimeout(() => fill.classList.add('fill'), 1);
@@ -23,7 +29,7 @@ function updateDisplay() {
 
 function markDone() {
 	done = true;
-	// endGame();
+	if (!firstMove) endGame();
 }
 
 const randomNumberGenarator = () => {
@@ -34,9 +40,8 @@ const newGame = () => {
 	box_1.innerText = randomNumberGenarator();
 	box_2.innerText = randomNumberGenarator();
 	score_val.innerHTML = score;
-	// progress_bar.style.width = '10%';
 	done = true;
-	updateDisplay();
+	if (!firstMove) updateDisplay();
 };
 
 const endGame = () => {
@@ -48,10 +53,16 @@ const endGame = () => {
 
 	yourScore.innerHTML = score;
 	highestScoreVal.innerHTML = highScore;
+
+	if (score == highScore) {
+		let gif = document.getElementById('gif');
+		gif.src = './resources/party_fox.gif';
+	}
 	showModal(endGameModal);
 };
 
 const check = (click_box) => {
+	firstMove = false;
 	let box_1_val = parseInt(box_1.innerHTML);
 	let box_2_val = parseInt(box_2.innerHTML);
 	if (
@@ -67,52 +78,88 @@ const check = (click_box) => {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-	if (event.target == instructionModal) {
+	if (event.target == shareModal) {
 		event.target.style.display = 'none';
 	}
 };
 
 const showModal = (modal) => {
 	modal.style.display = 'block';
+	if (modal == shareModal) {
+		sharedText = window.localStorage.getItem('shareText');
+		let highScore = localStorage.getItem('highScore');
+		if (sharedText == null || sharedText == '') {
+			let shareText = `Hi there, lets have a reflex check on 1sec. \nMy current high score: ${highScore}. \n\nURL : ${hostedUrl}`;
+			window.localStorage.setItem('shareText', shareText);
+			sharedText = window.localStorage.getItem('shareText');
+		}
+		shareTextBox.value = sharedText;
+	}
 };
 
 const hideModal = (modal) => {
 	modal.style.display = 'none';
 };
 
-showModal(instructionModal);
-newGame();
-
-progress.onclick = updateDisplay;
-fill.addEventListener('animationend', markDone);
-
 const startGame = (modal) => {
 	hideModal(modal);
 	score = 0;
-	timerFunction();
+	firstMove = true;
 	newGame();
 };
 
-// const timerFunction = () => {
-// 	var counter = 3;
+const mobileCheck = () => {
+	let check = false;
+	(function (a) {
+		if (
+			/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+				a
+			) ||
+			/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+				a.substr(0, 4)
+			)
+		)
+			check = true;
+	})(navigator.userAgent || navigator.vendor || window.opera);
+	return check;
+};
 
-// 	var timer = setInterval(function () {
-// 		// $('#countdown').remove();
+const feedbackRedirection = () => {
+	window.open('https://forms.gle/Eaejj4CNWEfCwx7c9');
+};
 
-// 		var countdown = $(
-// 			'<span id="countdown">' +
-// 				(counter == 0 ? 'SMILE!!' : counter) +
-// 				'</span>'
-// 		);
-// 		countdown.appendTo($('.container'));
-// 		setTimeout(() => {
-// 			if (counter > -1) {
-// 				$('#countdown').css({ 'font-size': '40vw', opacity: 0 });
-// 			} else {
-// 				$('#countdown').css({ 'font-size': '10vw', opacity: 50 });
-// 			}
-// 		}, 20);
-// 		counter--;
-// 		if (counter == -1) clearInterval(timer);
-// 	}, 1000);
-// };
+const share = (socialMedia) => {
+	sharedText = shareTextBox.value;
+	window.localStorage.setItem('shareText', sharedText);
+
+	navigator.clipboard.writeText(sharedText);
+
+	if (socialMedia == 'facebook') {
+		window.open(`http://www.facebook.com/sharer/sharer.php?u=${hostedUrl}`);
+	} else if (socialMedia == 'twitter') {
+		window.open(
+			`https://twitter.com/intent/tweet?url=${hostedUrl}&text=${sharedText}`
+		);
+	} else if (socialMedia == 'whatsapp') {
+		let check = mobileCheck();
+		if (check) {
+			window.open(`whatsapp://send?text=${sharedText}`);
+		} else {
+			window.open(`https://web.whatsapp.com://send?text=${sharedText}`);
+		}
+	} else if (socialMedia == 'clipboard') {
+		var tooltip = document.getElementById('myTooltip');
+		tooltip.innerHTML = 'Text copied!';
+	}
+};
+
+const outFunc = () => {
+	var tooltip = document.getElementById('myTooltip');
+	tooltip.innerHTML = 'Copy to clipboard';
+};
+
+showModal(instructionModal);
+// newGame();
+
+// progress.onclick = updateDisplay;
+fill.addEventListener('animationend', markDone);
